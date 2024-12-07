774469901af48424cd83fef4296f0d29cb2af8e7
Tsabit Gholib <tsabit1502@gmail.com>
2024-12-07 10:41:17 +0700

    fix qris

--- app/Http/Controllers/QrisController.php
+++ app/Http/Controllers/QrisController.php
@@ -195,7 +195,7 @@ class QrisController extends Controller
                     'message' => $billing->message,
                     'transaction_date' => now(),
                     'channel' => 'ONLINE',
-                    'va_number' => $billing->$vano,
+                    'va_number' => $vano,
                     'method' => 'QRIS',
                     'transaction_qr_id' => $transactionQrId,
                     'created_time' => $billing->created_time,
